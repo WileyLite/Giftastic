b37dcb2fdec6c29c6770273574a012dee3d71f4f
Jalin Wiley <jmt.wiley@gmail.com>
2019-10-23 18:23:48 -0400

    Jquery running and cross origin error fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,18 +5,23 @@ $(document).ready(function() {
 //gif url: api.giphy.com/v1/gifs/search
 //sticker url: api.giphy.com/v1/stickers/search
 //request url: https://api.giphy.com/v1/gifs/search?api_key=fhxV761nHZlyvKOCe8FSoC5J2sGD4Vwg&q=&limit=25&offset=0&rating=G&lang=en
-
-var xhr = $.get(
-  "https://api.giphy.com/v1/gifs/search?api_key=fhxV761nHZlyvKOCe8FSoC5J2sGD4Vwg&qfunny=&limit=25&offset=0&rating=G&lang=en"
-);
-xhr.done(function(data) {
-  console.log("success got data", data);
-
   //get search to work with html search button
+  $('button').on('click',function() {
+    var searchBtn = $(this).data("search");
+   console.log(searchBtn);
+
+var queryURL = 
+  "https://api.giphy.com/v1/gifs/search?api_key=fhxV761nHZlyvKOCe8FSoC5J2sGD4Vwg&q"+searchBtn+"=&limit=25&offset=0&rating=G&lang=en";
+// queryURL.done(function(data) {
+  console.log(queryURL);
+}); 
+
   //query server to run when search button is clicked
-  $.ajax({ url: xhr, method: "GET" }).done(function(response) {
+  $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
     console.log(response);
   });
   //push search to container where gifs will populate html
 });
-});
+
+
+
